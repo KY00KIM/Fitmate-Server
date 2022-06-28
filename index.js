2c79ab2709070cc258bcc1cee998ef89a01a594b
Kyumin-Kim <55953815+KY00KIM@users.noreply.github.com>
2022-06-28 14:54:47 +0900

    add folders

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const config = require('./config/config');
-
 const mongoose = require("mongoose");
 
-
-let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-    console.log('Connected to MongoDB');
-    server = app.listen(config.port, () => {
-        console.log(`Listening to port ${config.port}`);
-    });
-});
+const mongoURI = 'mongodb+srv://new-user-1:qwer@kdev-shop.eufbp0j.mongodb.net/?retryWrites=true&w=majority'
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user:1234@kdev-shop.eufbp0j.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(`mongoDB connected `)
+//     }
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
 
-const port = process.env.PORT || 5000
+//     client.close();
+// })
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+        uri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log(" Mongoose is connected")
+    );
+
+} catch (e) {
+    console.log("could not connect");
+}
+
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
+
+
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
     console.log(`Server Listening on ${port}`)
