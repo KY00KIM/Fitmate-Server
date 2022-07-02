59f138355e6e2952b08626cf05ed01d913988a9d
KyuminKim <hivedora@gmail.com>
2022-07-03 00:30:00 +0900

    [ADD] add firebase-authoriztion controller&config

--- controller/auth.js
+++ controller/auth.js
@@ -1,20 +1,40 @@
-const { User } = require('../models/User');
+const admin = require('../config/firebase-config');
+const { STATUS_CODE } = require('../config/http_status_code')
 
-let auth = (req, res, next) => {
-    let token = req.cookies.w_auth;
+//const { User } = require('../models/User');
 
-    User.findByToken(token, (err, user) => {
-        if (err) throw err;
-        if (!user)
-            return res.json({
-                isAuth: false,
-                error: true
-            });
+// let auth = (req, res, next) => {
+//     let token = req.cookies.w_auth;
 
-        req.token = token;
-        req.user = user;
-        next();
-    });
-};
+//     User.findByToken(token, (err, user) => {
+//         if (err) throw err;
+//         if (!user)
+//             return res.json({
+//                 isAuth: false,
+//                 error: true
+//             });
+
+//         req.token = token;
+//         req.user = user;
+//         next();
+//     });
+// };
+
+const auth = async (req, res, next) => {
+    const token = req.body.access_token;
+    try {
+        const decodeToken = admin.auth().verifyIdToken(token);
+        if (decodeToken) {
+            return next();
+        }
+        else {
+            return res.json({ code: STATUS_CODE.EMPTY_JWT, message: "Empty token" });
+        }
+    } catch (err) {
+        return res.json({ code: STATUS_CODE.INVALID_JWT, message: "Token validation error occured" });
+    }
+}
+
+
 
 module.exports = { auth };
--- index.js
+++ index.js
@@ -4,39 +4,9 @@ const path = require("path");
 const mongoose = require("mongoose");
 const router = require('./routes')
 
-const mongoURI = 'mongodb+srv://new-user-1:qwer@kdev-shop.eufbp0j.mongodb.net/?retryWrites=true&w=majority'
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://user:1234@kdev-shop.eufbp0j.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//     if (err) {
-//         console.log(err)
-//     } else {
-//         console.log(`mongoDB connected `)
-//     }
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
 
-//     client.close();
-// })
-// try {
-//     // Connect to the MongoDB cluster
-//     mongoose.connect(
-//         uri,
-//         { useNewUrlParser: true, useUnifiedTopology: true },
-//         () => console.log(" Mongoose is connected")
-//     );
-
-// } catch (e) {
-//     console.log("could not connect");
-// }
-
-
-// const dbConnection = mongoose.connection;
-// dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
-// dbConnection.once("open", () => console.log("Connected to DB!"));
 
 app.use('/', router)
 
