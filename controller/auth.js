ab9f3efa659672fb6c0b252725dc2457a9441bdb
KyuminKim <hivedora@gmail.com>
2022-07-05 02:00:25 +0900

    [ADD] add /controller/auth  jwt generation&authenication

--- controller/auth.js
+++ controller/auth.js
@@ -1,7 +1,10 @@
 const admin = require('../config/firebase-config');
-const { STATUS_CODE } = require('../config/http_status_code')
+const STATUS_CODE = require(path.join(__dirname, 'config/http_status_code'));
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const ResponseManager = require(path.join(__dirname, 'config/response'));
 
-//const { User } = require('../models/User');
+const { User } = require('../models/User');
 
 // let auth = (req, res, next) => {
 //     let token = req.cookies.w_auth;
@@ -34,6 +37,82 @@ const auth = async (req, res, next) => {
         return res.json({ code: STATUS_CODE.INVALID_JWT, message: "Token validation error occured" });
     }
 }
+
+/**
+ * 
+ * @param {headers: {authorization : "Bearer <JWT>"}} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns 
+ */
+
+
+const verifyToken = (req, res, next) => {
+    try {
+        const token = req.headers['authorization']
+        console.log(token)
+
+        req.decoded = jwt.verify(token, jwt_secret);
+        return next();
+    } catch (error) {
+        if (token == null) return res.status(401).json({
+            code: 401,
+            message: "EMPTY TOKEN"
+        });
+        if (error.name == 'TokenExpiredError') {
+            return ResponseManager.getDefaultResponseHandler(res)['onError']({});
+
+            return res.status(402).json({
+                code: 402,
+                message: "Token Out of date Expiration"
+            })
+        }
+        console.log(error)
+        return res.status(401).json({
+            code: 401,
+            message: 'Unvalid token',
+        });
+    }
+};
+
+
+
+const generateBothToken = (user) => {
+    /*
+    logic required for redundancy check with db
+    */
+    const refresh_token = jwt.sign(user, process.env.jwt_secret, { issuer: `FitMate Authenication Server` })
+    const access_token = generateExpiaryToken(user, '24h')
+
+    return {
+        access_token: access_token,
+        refresh_token: refresh_token
+    }
+
+}
+
+const generateAccessToken = (user, refresh_token) => {
+    /*
+    logic required for refresh_token validation check with db
+    */
+    return generateExpiaryToken(user, '24h');
+}
+
+
+/*
+기능 : access token 발행
+params : 
+    user : object that represents user to be signed
+    time : string for expiary time 
+        e.g.) "60s" "60m" "24h" "7d" 
+*/
+const generateExpiaryToken = (obj, time) => {
+    return jwt.sign(obj, PROCESS.ENV.JWT_SECRET, {
+        expiresIn: `${time}`,
+        issuer: `FitMate Authenication Server`
+    })
+}
+
 
 
 
