97551bc0373bbad0995723077ed767d8ec23aaf0
KyuminKim <hivedora@gmail.com>
2022-07-09 13:27:28 +0900

    [ADD] add controller/auth authenication

--- controller/auth.js
+++ controller/auth.js
@@ -1,119 +1,56 @@
 const admin = require('../config/firebase-config');
-const STATUS_CODE = require(path.join(__dirname, 'config/http_status_code'));
-const jwt = require("jsonwebtoken");
-const path = require("path");
-const ResponseManager = require(path.join(__dirname, 'config/response'));
+const ResponseManager = require('../config/response');
+const STATUS_CODE = require('../config/http_status_code');
+const User = require("../model/User");
 
-const { User } = require('../models/User');
 
-// let auth = (req, res, next) => {
-//     let token = req.cookies.w_auth;
+/**
+ * @description verify firebase token in request header
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns firebase user object in req.decoded 
+ */
 
-//     User.findByToken(token, (err, user) => {
-//         if (err) throw err;
-//         if (!user)
-//             return res.json({
-//                 isAuth: false,
-//                 error: true
-//             });
-
-//         req.token = token;
-//         req.user = user;
-//         next();
-//     });
-// };
-
-const auth = async (req, res, next) => {
-    const token = req.body.access_token;
+const verifyUser = async (req, res, next) => {
+    const token = req.header('Authorization')
     try {
         const decodeToken = admin.auth().verifyIdToken(token);
+        //토큰이 정상 복호화된 경우
         if (decodeToken) {
-            return next();
+            req.decoded = decodeToken
+            //DB에 등록되어 있고 활성화된 유저일 경우
+            if (getUserValidByToken(decodeToken))
+                return next();
+            //회원가입을 위한 요청일 경우
+            else if (req.originalUrl === "v1/user/oauth" && req.method === "POST") {
+                return next();
+                //DB에 등록되지 않았고 비활성화된 유저일 경우
+            } else {
+                ResponseManager.getDefaultResponseHandler(res)['onError']('INVALID_JWT', STATUS_CODE.INVALID_JWT);
+            }
         }
+        //토큰 내용이 없을 경우
         else {
-            return res.json({ code: STATUS_CODE.EMPTY_JWT, message: "Empty token" });
+            ResponseManager.getDefaultResponseHandler(res)['onError']('EMPTY_JWT', STATUS_CODE.EMPTY_JWT);
         }
+        //firebase 토큰 인증 메서드 오류
     } catch (err) {
-        return res.json({ code: STATUS_CODE.INVALID_JWT, message: "Token validation error occured" });
+        console.log(err)
+        ResponseManager.getDefaultResponseHandler(res)['onError']('INVALID_JWT', STATUS_CODE.INVALID_JWT);
     }
 }
 
 /**
- * 
- * @param {headers: {authorization : "Bearer <JWT>"}} req 
- * @param {*} res 
- * @param {*} next 
- * @returns 
+ * @description Check integrity of firebase token with UserDB
+ * @param {*} Token Decoded firebase token
+ * @returns Boolean whether user exists and is valid
  */
-
-
-const verifyToken = (req, res, next) => {
-    try {
-        const token = req.headers['authorization']
-        console.log(token)
-
-        req.decoded = jwt.verify(token, jwt_secret);
-        return next();
-    } catch (error) {
-        if (token == null) return res.status(401).json({
-            code: 401,
-            message: "EMPTY TOKEN"
-        });
-        if (error.name == 'TokenExpiredError') {
-            return ResponseManager.getDefaultResponseHandler(res)['onError']({});
-
-            return res.status(402).json({
-                code: 402,
-                message: "Token Out of date Expiration"
-            })
-        }
-        console.log(error)
-        return res.status(401).json({
-            code: 401,
-            message: 'Unvalid token',
-        });
-    }
-};
-
-
-
-const generateBothToken = (user) => {
-    /*
-    logic required for redundancy check with db
-    */
-    const refresh_token = jwt.sign(user, process.env.jwt_secret, { issuer: `FitMate Authenication Server` })
-    const access_token = generateExpiaryToken(user, '24h')
-
-    return {
-        access_token: access_token,
-        refresh_token: refresh_token
-    }
-
-}
-
-const generateAccessToken = (user, refresh_token) => {
-    /*
-    logic required for refresh_token validation check with db
-    */
-    return generateExpiaryToken(user, '24h');
+const getUserValidByToken = async (Token) => {
+    const userId = Token.uid;
+    const user = await User.find({ 'social.user_id': userId });
+    return user && !user.is_deleted
 }
 
 
-/*
-기능 : access token 발행
-params : 
-    user : object that represents user to be signed
-    time : string for expiary time 
-        e.g.) "60s" "60m" "24h" "7d" 
-*/
-const generateExpiaryToken = (obj, time) => {
-    return jwt.sign(obj, PROCESS.ENV.JWT_SECRET, {
-        expiresIn: `${time}`,
-        issuer: `FitMate Authenication Server`
-    })
-}
-
-
-
-
-module.exports = { auth };
+module.exports = { verifyUser };
