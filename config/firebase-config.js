9ec2ef8eabb6fc433b4795b61e980f2bf89123b6
Chocochip101 <fingercut3822@gmail.com>
2022-07-10 16:44:28 +0900

    [Fix] Capital

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./ServiceAccount.json");
+const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
