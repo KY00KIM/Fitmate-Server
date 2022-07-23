d204a362bf40c24d88a67749eeaf567017008d22
KyuminKim <hivedora@gmail.com>
2022-07-24 04:19:57 +0900

    [ADD] add watch UserTrace

--- controller/match.js
+++ controller/match.js
@@ -1,7 +1,7 @@
 const { Appointment } = require('../model/Appointment');
 const { User } = require('../model/User');
 const { UserTrace } = require('../model/UserTrace');
-const {pushData} = require('./push');
+const { pushData } = require('./push');
 const schedule = require('node-schedule');
 const ResponseManager = require('../config/response');
 const STATUS_CODE = require('../config/http_status_code');
@@ -37,7 +37,7 @@ const matchController = {
             else {
                 const appointment = await Appointment.findByIdAndUpdate(appointmentId, { match_succeeded: false }, { new: true, runValidators: true });
                 data = {
-                    "appointmentId":appointment._id,
+                    "appointmentId": appointment._id,
                     Type: "QRCODE"
                 };
 
@@ -53,33 +53,42 @@ const matchController = {
 
                 const match_start_user = await User.findById(user_1.user_id);
                 const match_join_user = await User.findById(user_2.user_id);
-                
-                match_start_user.social.device_token.forEach((deviceToken)=>{
-                    schedule.scheduleJob(rule,() => pushData(deviceToken, data));
-                  });
-                  match_join_user.social.device_token.forEach((deviceToken) => {
-                    schedule.scheduleJob(rule,() => pushData(deviceToken, data));
-                  });
+
+                match_start_user.social.device_token.forEach((deviceToken) => {
+                    schedule.scheduleJob(rule, () => pushData(deviceToken, data));
+                });
+                match_join_user.social.device_token.forEach((deviceToken) => {
+                    schedule.scheduleJob(rule, () => pushData(deviceToken, data));
+                });
                 ResponseManager.getDefaultResponseHandler(res)['onSuccess'](appointment, 'SuccessOK', STATUS_CODE.SuccessOK);
             }
         } catch (error) {
             ResponseManager.getDefaultResponseHandler(res)['onError'](error, 'ClientErrorBadRequest', STATUS_CODE.ClientErrorBadRequest);
         }
+    },
+
+    getDistanceFromLatLonInKm: (lat1, lng1, lat2, lng2) => {
+        function deg2rad(deg) {
+            return deg * (Math.PI / 180)
+        }
+
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+        var dLon = deg2rad(lng2 - lng1);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c; // Distance in km
+        return d;
+    },
+
+
+    getDistanceBetweenUsersInKm: async (user_id1, user_id2) => {
+        const user_1 = await User.findById(user_id1);
+        const user_2 = await User.findById(user_id2);
+        return getDistanceFromLatLonInKm(user_1.user_latitude, user_1.user_longitude, user_2.user_latitude, user_2.user_longitude)
     }
 };
 
-function getDistanceFromLatLonInKm(lat1, lng1, lat2, lng2) {
-    function deg2rad(deg) {
-        return deg * (Math.PI / 180)
-    }
 
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-    var dLon = deg2rad(lng2 - lng1);
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;
-}
 
 module.exports = matchController;
