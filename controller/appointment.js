9a9005403edbf331b23ae493dea3bddcc5d222f0
KyuminKim <hivedora@gmail.com>
2022-07-24 13:23:36 +0900

    [ADD] add user-trace-watch / fixed postAPI & swagger

--- controller/appointment.js
+++ controller/appointment.js
@@ -1,6 +1,6 @@
-const {Appointment} = require('../model/Appointment');
-const {User} = require('../model/User');
-const {PushSchedule} = require('../model/PushSchedule');
+const { Appointment } = require('../model/Appointment');
+const { User } = require('../model/User');
+const { PushSchedule } = require('../model/PushSchedule');
 
 const ResponseManager = require('../config/response');
 const schedule = require('node-schedule');
@@ -8,7 +8,7 @@ const STATUS_CODE = require('../config/http_status_code');
 const moment = require('moment');
 const fitnesscenterController = require('./fitnesscenter');
 const timeConvert = require('../config/timeConvert');
-const {pushNotification, pushData} = require('./push');
+const { pushNotification, pushData } = require('./push');
 
 const appointmentController = {
   /**
@@ -16,13 +16,13 @@ const appointmentController = {
    * @description 사용자의 모든 약속을 조회하는 GET Method
    */
   /// userId 필요
-  getAllAppointment : async (req, res) => {
+  getAllAppointment: async (req, res) => {
     try {
       let user_id = req.user.id;
       const appointments = await Appointment.find(
-        { $or:[{'match_start_id':user_id}, {'match_join_id':user_id} ]});
+        { $or: [{ 'match_start_id': user_id }, { 'match_join_id': user_id }] });
 
-      appointments.forEach((appointment) =>{        
+      appointments.forEach((appointment) => {
         appointment.appointment_date = timeConvert.addNineHours(appointment.appointment_date);
         appointment.createdAt = timeConvert.addNineHours(appointment.createdAt);
         appointment.updatedAt = timeConvert.addNineHours(appointment.updatedAt);
@@ -39,18 +39,18 @@ const appointmentController = {
    */
 
   getOneAppointment: async (req, res) => {
-      try {
-          const {
-              params: { appointmentId },
-            } = req;
-          const appointment = await Appointment.findById(appointmentId).populate('match_start_id').populate('match_join_id');
-          appointment.appointment_date = timeConvert.addNineHours(appointment.appointment_date);
-          appointment.createdAt = timeConvert.addNineHours(appointment.createdAt);
-          appointment.updatedAt = timeConvert.addNineHours(appointment.updatedAt);
-          ResponseManager.getDefaultResponseHandler(res)['onSuccess'](appointment, 'SuccessOK', STATUS_CODE.SuccessOK);
-      }catch(error){
-          ResponseManager.getDefaultResponseHandler(res)['onError'](error, 'ClientErrorNotFound', STATUS_CODE.ClientErrorNotFound);
-      }
+    try {
+      const {
+        params: { appointmentId },
+      } = req;
+      const appointment = await Appointment.findById(appointmentId).populate('match_start_id').populate('match_join_id');
+      appointment.appointment_date = timeConvert.addNineHours(appointment.appointment_date);
+      appointment.createdAt = timeConvert.addNineHours(appointment.createdAt);
+      appointment.updatedAt = timeConvert.addNineHours(appointment.updatedAt);
+      ResponseManager.getDefaultResponseHandler(res)['onSuccess'](appointment, 'SuccessOK', STATUS_CODE.SuccessOK);
+    } catch (error) {
+      ResponseManager.getDefaultResponseHandler(res)['onError'](error, 'ClientErrorNotFound', STATUS_CODE.ClientErrorNotFound);
+    }
   },
 
 
@@ -61,7 +61,7 @@ const appointmentController = {
   writeAppointment: async (req, res) => {
     try {
       const {
-        body: {fitness_center, match_start_id, match_join_id, appointment_date},
+        body: { fitness_center, match_start_id, match_join_id, appointment_date },
       } = req;
       const fitness_center_id = await fitnesscenterController.getFitnessCenterId(fitness_center);
 
@@ -78,9 +78,9 @@ const appointmentController = {
       let rule = new schedule.RecurrenceRule();
       let new_date = moment(appointment_date).add(1, 'days');
 
-      rule.year = moment(new_date).year();        
-      rule.month = moment(new_date).month() + 1;  
-      rule.date = moment(new_date).date();    
+      rule.year = moment(new_date).year();
+      rule.month = moment(new_date).month() + 1;
+      rule.date = moment(new_date).date();
 
       rule.hour = 0;
       rule.minute = 0;
@@ -88,54 +88,54 @@ const appointmentController = {
       console.log(rule);
 
       // 리뷰 요청 알림 예약
-      match_start_user.social.device_token.forEach((deviceToken)=>{
-        schedule.scheduleJob(rule,() => pushNotification(deviceToken, 'FitMate 리뷰 알림!' ,`${match_join_user.user_nickname}님과의 운동은 어떠셨나요?`));
+      match_start_user.social.device_token.forEach((deviceToken) => {
+        schedule.scheduleJob(rule, () => pushNotification(deviceToken, 'FitMate 리뷰 알림!', `${match_join_user.user_nickname}님과의 운동은 어떠셨나요?`));
       });
       match_join_user.social.device_token.forEach((deviceToken) => {
-        schedule.scheduleJob(rule,() => pushNotification(deviceToken,'FitMate 리뷰 알림!' ,`${match_join_user.user_nickname}님과의 운동은 어떠셨나요?`));
+        schedule.scheduleJob(rule, () => pushNotification(deviceToken, 'FitMate 리뷰 알림!', `${match_join_user.user_nickname}님과의 운동은 어떠셨나요?`));
       });
 
       // DB에 저장
       await PushSchedule.create({
         pushType: "REVIEW",
-        appointmentId:appointment._id,
+        appointmentId: appointment._id,
         match_start_id: match_start_id,
         match_join_id: match_join_id,
         rule: new_date
       });
 
       // GPS 요청 정보 예약
-      rule.year = moment(appointment_date).year();        
-      rule.month = moment(appointment_date).month() + 1;  
-      rule.date = moment(appointment_date).date();    
+      rule.year = moment(appointment_date).year();
+      rule.month = moment(appointment_date).month() + 1;
+      rule.date = moment(appointment_date).date();
       rule.hour = moment(appointment_date).hour();
       rule.minute = moment(appointment_date).minute() + 5;
       rule.second = moment(appointment_date).second();
 
       data = {
-        "appointmentId":appointment._id,
+        "appointmentId": appointment._id,
         Type: "GPS"
       }
-      match_start_user.social.device_token.forEach((deviceToken)=>{
-        schedule.scheduleJob(rule,() => pushData(deviceToken, data));
+      match_start_user.social.device_token.forEach((deviceToken) => {
+        schedule.scheduleJob(rule, () => pushData(deviceToken, data));
       });
       match_join_user.social.device_token.forEach((deviceToken) => {
-        schedule.scheduleJob(rule,() => pushData(deviceToken, data));
+        schedule.scheduleJob(rule, () => pushData(deviceToken, data));
       });
 
       // DB에 저장
       await PushSchedule.create({
         pushType: "GPS",
-        appointmentId:appointment._id,
+        appointmentId: appointment._id,
         match_start_id: match_start_id,
         match_join_id: match_join_id,
         rule: appointment_date
       });
-      
+
       ResponseManager.getDefaultResponseHandler(res)['onSuccess'](appointment, 'SuccessCreated', STATUS_CODE.SuccessCreated);
 
-    } catch (error) {    
-      console.error(error);   
+    } catch (error) {
+      console.error(error);
       ResponseManager.getDefaultResponseHandler(res)['onError'](error, 'ClientErrorNotFound', STATUS_CODE.ClientErrorNotFound);
     }
   }
--- model/UserTrace.js
+++ model/UserTrace.js
@@ -1,24 +1,52 @@
 const mongoose = require('mongoose');
 require('mongoose-double')(mongoose);
 const { Types: { ObjectId, Double } } = mongoose.Schema;
+const { User } = require('./User')
+const { Appointment } = require('./Appointment')
+const matchController = require('../controller/match')
 
 
 const userTraceSchema = mongoose.Schema({
-    user_id: {
-        type: ObjectId,
-        ref: 'User',
-      },
-    user_longitude: Double,
-    user_latitude: Double,
-  }, {
-    versionKey: false,
-    timestamps: true,
-    toJSON: { virtuals: true }
+  user_id: {
+    type: ObjectId,
+    ref: 'User',
+  },
+  user_longitude: Double,
+  user_latitude: Double,
+}, {
+  versionKey: false,
+  timestamps: true,
+  toJSON: { virtuals: true }
+});
+
+const UserTrace = mongoose.model('UserTrace', userTraceSchema);
+
+
+UserTrace.watch().
+  on('change', async (data) => {
+    const trace = await UserTrace.findById(data.documentKey)
+    //UPDATE USER LON, LAT FROM TRACE
+    const user = await User.findByIdAndUpdate(trace.user_id, {
+      user_latitude: trace.user_latitude,
+      user_longitude: trace.user_longitude
+    })
+
+    //get appointments within 10min & check Matching
+    const curTime = new Date().getTime()
+    const appointments = await Appointment.find({
+      $or: [{ 'match_start_id': trace.user_id }, { 'match_join_id': trace.user_id }],
+      appointment_date: { $gte: curTime - 600000, $lte: curTime + 600000 }
+    });
+    const appointment = appointments[0]
+    if (appointments[0]) {
+      const distance = matchController.getDistanceBetweenUsersInKm(appointment.match_start_id, appointment.match_join_id)
+      if (distance <= 1) {
+        //MATCH SUCCEED
+        await Appointment.findByIdAndUpdate(appointment._id, { match_succeeded: true });
+      }
+    }
   });
-  
-  const UserTrace = mongoose.model('UserTrace', userTraceSchema);
-  
-  module.exports = { UserTrace };
-  
-  
-  
+
+module.exports = { UserTrace };
+
+
