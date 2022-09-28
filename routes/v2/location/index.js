82b541d62f63f7be9c7840ff04358c36299bd7fd
Chocochip101 <fingercut3822@gmail.com>
2022-09-28 15:46:03 +0900

    [HOTFIX]

--- routes/v2/location/index.js
+++ routes/v2/location/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const locationRouter = express.Router();
 const locationController = require('../../../controller/location');
 
-const { verifyUser } = require("../../../middleware/auth");
 locationRouter.get('/', locationController.getAllLocation);
 locationRouter.get('/:locId', locationController.getOneLocation);
 locationRouter.post('/:locId', locationController.updateOneLocation);
